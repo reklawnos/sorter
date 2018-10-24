81b9974c7e238b0806125a2352b2c001ad227861
Walker Henderson <wjahenderson@gmail.com>
2018-10-23 21:11:30 -0700

    Updates

---
+++ precache-manifest.92844f5dd057281a46f4f57a344aff86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c4652a55912ab7432b2",
+    "url": "/sorter/static/js/runtime~main.4c4652a5.js"
+  },
+  {
+    "revision": "1f159a81e753ad9b8c33",
+    "url": "/sorter/static/js/main.1f159a81.chunk.js"
+  },
+  {
+    "revision": "0d04f68481c8318c0269",
+    "url": "/sorter/static/js/1.0d04f684.chunk.js"
+  },
+  {
+    "revision": "1f159a81e753ad9b8c33",
+    "url": "/sorter/static/css/main.545b281e.chunk.css"
+  },
+  {
+    "revision": "cf795a224c1c9a7879bb6fc78c0de0a2",
+    "url": "/sorter/index.html"
+  }
+];
